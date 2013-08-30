3725d6937d15f79c1671b448213025c8f4468db7
lushh <lushouheng@gmail.com>
2013-08-30 15:36:12 +0800

    ds

--- app.js
+++ app.js
@@ -10,4 +10,4 @@ http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/plain'});
     res.end('Hello World\nThis is my first NodeJs');
 }).listen(2222,'127.0.0.1');
-console.log("Server running at http:127.0.0.1:2222");
+console.log("Serverr running at http:127.0.0.1:2222");
